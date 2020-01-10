759f91bc1ff36399ee5553e01e85c1fa01d5e90c
Majcew <48514316+Majcew@users.noreply.github.com>
2020-01-10 18:20:30 +0100

    problem z rysowaniem na canvasie przy zmianie rozmiarow

--- script.js
+++ script.js
@@ -1,9 +1,52 @@
 window.onload=function(){
+    can = document.querySelector("canvas");
+    q = can.getContext("2d");
+    can.addEventListener('touchstart',startDotyku,true);
+	can.addEventListener('touchend',stopDotyku,true);
+	can.addEventListener('touchmove',ruchDotyku,true);
     document.getElementById("resolution").innerHTML = window.innerWidth + "px x "+ window.innerHeight+"px";
     sizeOfPen();
     sizeOfCanvas();
 }
-
+var tablicaDotyk=[];
+var q;
+var can;
+function startDotyku(e)
+{
+    tablicaDotyk.length=0;	
+	for(var i=0; i<e.changedTouches.length; i++)
+	{
+		var x=e.changedTouches[i].clientX;
+	    var y=e.changedTouches[i].clientY;
+        tablicaDotyk.push({x:x,y:y});
+	}
+	e.preventDefault();
+	e.stopPropagation()
+}
+function stopDotyku()
+{
+    tablicaDotyk.length=0;
+}
+function ruchDotyku(e)
+{
+    var x1=can.getBoundingClientRect().left;
+	var y1=can.getBoundingClientRect().top;
+	
+	for(var i=0; i<e.changedTouches.length && i<tablicaDotyk.length; i++)
+	{
+		var x=e.changedTouches[i].pageX;
+		var y=e.changedTouches[i].pageY;
+		q.beginPath();
+		q.moveTo(tablicaDotyk[i].x-x1,tablicaDotyk[i].y-y1);
+		q.lineWidth=7;
+		q.lineTo(x-x1, y-y1);
+		q.stroke();
+		tablicaDotyk[i].x=x;
+		tablicaDotyk[i].y=y;
+	}
+	e.preventDefault();
+	e.stopPropagation()
+}
 function sizeOfPen(){
     var sliderPen = document.getElementById("sizePen");
 	var outputPen = document.getElementById("valuesPen");
